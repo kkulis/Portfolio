5fedb87306b69819b027af296a789219926f09c0
krzysiekkulig <Krzysiek.kulig@gmail.com>
2020-05-08 17:02:28 +0200

    New CV

--- src/Resume.js
+++ src/Resume.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
 function Resume() {
-    return(
+    return (
         <section id="resume" class="main special">
-        <header class="major">
-            <h2>Resume</h2>
-        </header>
-        <footer class="major">
-                <a href="https://drive.google.com/open?id=10Jt2_cIAxtNlUNvAb6EOaNgpsq5r-vQM" download class="button primary">Download</a>
-        </footer>
-    </section>
-    )
+            <header class="major">
+                <h2>Resume</h2>
+            </header>
+            <footer class="major">
+                <a
+                    href="https://drive.google.com/open?id=1EWtxckb7M_xECfQrp8rhXvtUy70uE_5n"
+                    download
+                    class="button primary"
+                >
+                    Download
+                </a>
+            </footer>
+        </section>
+    );
 }
 
 export default Resume;
