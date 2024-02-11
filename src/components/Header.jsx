b8717c5e206e54792af6ad1eea34e58b55580d61
Krzysiek Kulig <krzysiek.kulig@gmail.com>
2024-02-11 12:47:33 +0100

    Header styling update, slide in add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,44 @@
 import './../styles/Header.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import avatar from './../images/portfolio_image.jpeg';
 
-export const Header = () => (
-    <div>
-        <header id="header" className="alt">
-            <span className="logo">
-                <img src={avatar} alt="avatar"></img>
-            </span>
-            <h1>Krzysiek Kulig</h1>
-            <p>
-                Frontend-preferring full stack developer. Image processing
-                passionate with a weakness for python.
-            </p>
-            <ul className="icons">
-                <li>
-                    <a
-                        href="https://github.com/kkulis"
-                        className="icon brands fa-github alt"
-                    >
-                        <span className="label">GitHub</span>
-                    </a>
-                </li>
-                <li>
-                    <a
-                        href="https://www.linkedin.com/in/krzysztof-kulig-3965781a5/"
-                        className="icon brands fa-linkedin alt"
-                    >
-                        <span className="label">LinkedIn</span>
-                    </a>
-                </li>
-            </ul>
-        </header>
-    </div>
-);
+export const Header = () => {
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        setIsLoaded(true);
+    }, []);
+
+    return (
+        <div className={`header-content ${isLoaded ? 'loaded' : ''} `}>
+            <header id="header" className="alt">
+                <div className="logo">
+                    <img src={avatar} alt="avatar"></img>
+                </div>
+                <h1>Krzysiek Kulig</h1>
+                <p>
+                    Frontend-preferring full stack developer. Image processing
+                    passionate with a weakness for python.
+                </p>
+                <ul className="icons">
+                    <li>
+                        <a
+                            href="https://github.com/kkulis"
+                            className="icon brands fa-github alt"
+                        >
+                            <span className="label">GitHub</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://www.linkedin.com/in/krzysztof-kulig-3965781a5/"
+                            className="icon brands fa-linkedin alt"
+                        >
+                            <span className="label">LinkedIn</span>
+                        </a>
+                    </li>
+                </ul>
+            </header>
+        </div>
+    );
+};
